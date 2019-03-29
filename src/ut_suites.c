cd5b0051abd31b10418c8ecb228b5c4c10d9d813
Corey Tessler <corey.tessler@wayne.edu>
2019-03-29 11:52:14 -0400

    Update to CUNIT version 2.1_p3

--- src/ut_suites.c
+++ src/ut_suites.c
@@ -36,12 +36,12 @@ extern int ut_dl_cleanup(void);
 
 
 CU_SuiteInfo suites[] = {
-    { "CUNIT", cunit_init, cunit_cleanup, cunit_tests },
-    { "TASK", task_init, task_cleanup, task_tests },
-    { "TASKSETS", taskset_init, taskset_cleanup, taskset_tests },    
-    { "TPJ", tpj_init, tpj_cleanup, tpj_tests },
-    { "ORDL", ut_ordl_init, ut_ordl_cleanup, ordl_tests },
-    { "DEADLINES", ut_dl_init, ut_dl_cleanup, ut_dl_tests },
+    { "CUNIT", cunit_init, cunit_cleanup, NULL, NULL, cunit_tests },
+    { "TASK", task_init, task_cleanup, NULL, NULL, task_tests },
+    { "TASKSETS", taskset_init, taskset_cleanup, NULL, NULL, taskset_tests },    
+    { "TPJ", tpj_init, tpj_cleanup, NULL, NULL, tpj_tests },
+    { "ORDL", ut_ordl_init, ut_ordl_cleanup, NULL, NULL, ordl_tests },
+    { "DEADLINES", ut_dl_init, ut_dl_cleanup, NULL, NULL, ut_dl_tests },
     CU_SUITE_INFO_NULL
 };
 
