a8959dd819f36381683981f923e451d14116e0b0
Corey Tessler <corey.tessler@wayne.edu>
2019-07-10 11:23:34 -0400

    Updated helper scripts

--- dtask2pdf.sh
+++ dtask2pdf.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 tex=${1%.*}.tex
 pdf=${1%.*}.pdf
+new=${2:-${pdf}}
 dot2tex -tmath -p --autosize $1 > $tex
 latexmk -pdf $tex
 latexmk -c $tex
+mv $pdf $new
--- visualize.sh
+++ visualize.sh
@@ -2,22 +2,26 @@
 
 GSL_RNG_SEED=`date +%s`
 
-GSL_RNG_TYPE=ranlxs2 GSL_RNG_SEED=`date +%s` bin/dts-gen-nodes -n 6 -e 0.2 -o vis-01.dot
-dot2tex -tmath -p --autosize vis-01.dot > vis-01.tex
-pdflatex vis-01.tex
+GSL_RNG_TYPE=ranlxs2 GSL_RNG_SEED=`date +%s` \
+    bin/dts-gen-nodes -n 6 -e 0.2 -o vis-01.dot
+./dtask2pdf.sh vis-01.dot
 
 (( ++GSL_RND_SEED ))
 GSL_RNG_TYPE=ranlxs2 GSL_RNG_SEED=`date +%s` \
     bin/dts-demand -t vis-01.dot -w 50 -j 3 -f 0.7 -o vis-02.dot
-dot2tex -tmath -p --autosize vis-02.dot > vis-02.tex
-pdflatex vis-02.tex
+./dtask2pdf.sh vis-02.dot
 
 GSL_RNG_TYPE=ranlxs2 GSL_RNG_SEED=`date +%s` \
-	    bin/dts-period -t vis-02.dot -o vis-03.dot -u 0.6
-dot2tex -tmath -p --autosize vis-03.dot > vis-03.tex
-pdflatex vis-03.tex
+    bin/dts-period -t vis-02.dot -o vis-03.dot -u 0.6
+./dtask2pdf.sh vis-03.dot
 
 GSL_RNG_TYPE=ranlxs2 GSL_RNG_SEED=`date +%s` \
-	    bin/dts-deadline -t vis-03.dot -o vis-04.dot -b
-dot2tex -tmath -p --autosize vis-04.dot > vis-04.tex
-pdflatex vis-04.tex
+    bin/dts-deadline -t vis-03.dot -o vis-04.dot -b
+./dtask2pdf.sh vis-04.dot
+
+# Arbitrary order of candidates for collapse
+bin/dts-cand-order -t vis-04.dot -a -o cand.list
+
+# Collapse all the beneficial nodes
+bin/dts-collapse-list -L cand.list vis-04.dot -o vis-05.dot
+./dtask2pdf.sh vis-05.dot
